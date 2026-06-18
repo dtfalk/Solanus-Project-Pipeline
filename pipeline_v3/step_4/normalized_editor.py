12736fe58241ebdbfa4f5e87639e379661cc45e3
David <davidtobiasfalk@gmail.com>
2026-06-17 20:11:14 -0400

    fix intro pages formatting and labelling

--- pipeline_v3/step_4/normalized_editor.py
+++ pipeline_v3/step_4/normalized_editor.py
@@ -82,7 +82,7 @@ from pdf2image import convert_from_path
 from PIL import ImageTk
 
 # Document we are intending to review/edit
-TARGET_DOCUMENT = "Volume_3"
+TARGET_DOCUMENT = "Volume_4"
 
 SCRIPT_DIR = Path(__file__).resolve().parent
 
